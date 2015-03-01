65f1c552f18fbf18cf74b4ad849f31ac8c31a397
Maxim Kustov <maksym.kustov@gmail.com>
2015-03-01 13:05:09 +0200

    Fix index action.

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -2,7 +2,7 @@ require 'currency_api'
 
 class CurrenciesController < ApplicationController
   def index
-    currency_name = params[:currency][:currency_name]
+    currency_name = params[:currency][:currency_name] if params[:currency].present?
     @currencies = currency_name.present? ? CurrencyAPI.get_currency(currency_name) : CurrencyAPI.get_currencies
   end
 end
