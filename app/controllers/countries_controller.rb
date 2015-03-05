8a55bbd7356edb72cb32c7872d0662db74a6d0dd
Maxim Kustov <maksym.kustov@gmail.com>
2015-03-05 18:46:24 +0200

    Mod: visit country via ajax.

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,13 +1,10 @@
 class CountriesController < ApplicationController
   inherit_resources
+  respond_to :js, :html
   def index
     filter = params[:filter][:currency] if params[:filter].present?
     @countries = filter.present? ? Country.includes(:currency).where(currency_id: filter) : Country.includes(:currency).all
     @countries.order!(:name)
-  end
-
-  def update
-    update! { countries_path }
   end
 
   def load_data
