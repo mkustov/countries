3d108b70f19f2e48f457b4e1c9cfe91b3742b735
Maxim Kustov <maksym.kustov@gmail.com>
2015-03-07 12:50:39 +0200

    Rm: currencies views

--- app/controllers/currencies_controller.rb
+++
@@ -1,8 +0,0 @@
-require 'currency_api'
-
-class CurrenciesController < ApplicationController
-  def index
-    currency_name = params[:currency][:currency_name] if params[:currency].present?
-    @currencies = currency_name.present? ? CurrencyAPI.get_currency(currency_name) : CurrencyAPI.get_currencies
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   mount Sidekiq::Web, at: '/sidekiq'
   root 'countries#index'
-  resources :currencies
   resources :countries do
     post 'load_data', on: :collection
   end
